8691f616acf5c8ce40046d469c7e2c56b8ccace6
Roland Clairquin <rolandcl@azrael.fontaine>
2018-02-01 23:28:25 +0100

    Travail sur git-init et git-hash-object

--- src/git_in_clojure/core.clj
+++ src/git_in_clojure/core.clj
@@ -3,14 +3,32 @@
   (:require [clojure.java.io :as io])
   (:gen-class))
 
+(def GIT-DIR "git-test-dir")
+
 (defn git-object
   [object-type content]
   (format "%s %d\0%s" object-type (count content) content))
 
+(defn git-init
+  ([] (git-init "."))
+  ([root]
+   (let [path (io/file root GIT-DIR)]
+     (if (.exists path)
+       (throw (RuntimeException. "Le dépot GIT existe déjà"))
+       (io/make-parents root GIT-DIR "description")))))
+
 (defn git-hash-object
   [content & {:keys [type]
               :or {type "blob"}}]
-  [content type])
+  (let [c-bytes (cond (string? content) (.getBytes content)
+                      (-> content class .getComponentType (= Byte/TYPE)) content)
+        h (sha1 (java.io.SequenceInputStream.
+                 (-> (format "%s %d\0" type (count c-bytes))
+                     .getBytes
+                     java.io.ByteArrayInputStream.)
+                 (-> c-bytes
+                     java.io.ByteArrayInputStream.)))]
+    h))
 
 (defn decompress-file
   []
@@ -18,6 +36,7 @@
                       io/input-stream
                       java.util.zip.InflaterInputStream.)]
     (slurp xin)))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
